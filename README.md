d0cf231ecc252eea29dc04130e0f4193d7b92455
Laura <140553486+Laauraaa@users.noreply.github.com>
2023-09-07 21:47:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# atividade-algoritmo
+ Primeiro trabalho de algoritmos, o professor solicitou uma atividade com média e outra com média + switch
---
+++ script.js
@@ -0,0 +1,52 @@
+function calcular() {
+    var pt = document.getElementById('npt')
+    var st = document.getElementById('nst')
+    var p = document.getElementById('np')
+    if (pt.value > 10 || pt.value < 0 ) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    } if (st.value > 10 || st.value < 0) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    } if (p.value > 10 ||  p.value < 0) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    }
+     else {
+        var ppt = Number(pt.value)
+        var sst = Number(st.value)
+        var pp = Number(p.value)
+        res = (ppt + sst + pp)/3
+        resultA.innerHTML = `${res}`
+    }
+}
+
+ function verificar() {
+    var tum = document.getElementById('ntum')
+    var tdois = document.getElementById('ntdois')
+    var ttres = document.getElementById('nttres')
+    if (tum.value < 0 || tum.value > 10) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    } if (tdois.value < 0 || tdois.value > 10) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    } if (ttres.value < 0 || ttres.value > 10) {
+        window.alert('Erro! Os dados estão incorretos, insira novamente')
+    } else {
+        var tu = Number(tum.value)
+        var td = Number(tdois.value)
+        var tt = Number(ttres.value)
+        var res = (tu + td + tt)/3
+        if (res <= 5 ) {
+            x = 'REPROVADO'
+        } else {
+            x = 'APROVADO'
+        }
+    switch (x) {
+        case 'APROVADO':
+            resultB.innerHTML = `Sua nota é ${res}, você foi aprovado!`
+        break
+        case 'REPROVADO':
+            resultB.innerHTML = `Sua nota é ${res}, infelizmente você foi reprovado.`
+        break
+        default:
+            resultB.innerHTML = "?"
+    }
+    }
+ }
