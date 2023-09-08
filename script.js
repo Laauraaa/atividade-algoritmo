8c25716c380da11ab518b5419654b4ce13130058
Laura <140553486+Laauraaa@users.noreply.github.com>
2023-09-07 22:24:57 -0300

    correção de erros se sintaxe e lógica

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function calcular() {
     var st = document.getElementById('nst')
     var p = document.getElementById('np')
     if (pt.value > 10 || pt.value < 0 ) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
-    } if (st.value > 10 || st.value < 0) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
-    } if (p.value > 10 ||  p.value < 0) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
+        resultA.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
+    } else if (st.value > 10 || st.value < 0) {
+        resultA.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
+    } else if (p.value > 10 ||  p.value < 0) {
+        resultA.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
     }
      else {
         var ppt = Number(pt.value)
         var sst = Number(st.value)
         var pp = Number(p.value)
         res = (ppt + sst + pp)/3
-        resultA.innerHTML = `${res}`
+        resultA.innerHTML = `Sua nota é: ${res}`
     }
 }
 
@@ -23,11 +23,11 @@ function calcular() {
     var tdois = document.getElementById('ntdois')
     var ttres = document.getElementById('nttres')
     if (tum.value < 0 || tum.value > 10) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
-    } if (tdois.value < 0 || tdois.value > 10) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
-    } if (ttres.value < 0 || ttres.value > 10) {
-        window.alert('Erro! Os dados estão incorretos, insira novamente')
+        resultB.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
+    } else if (tdois.value < 0 || tdois.value > 10) {
+        resultB.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
+    } else if (ttres.value < 0 || ttres.value > 10) {
+        resultB.innerHTML ='Erro! Os dados estão incorretos, insira novamente'
     } else {
         var tu = Number(tum.value)
         var td = Number(tdois.value)
